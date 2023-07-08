7939e5bdb1aa4c004d64451d2ad712cfaabc2f85
Minal D. Patil <88721502+minal3105@users.noreply.github.com>
2023-07-08 21:57:57 +0530

    ok
    ok

---
+++ SalaryManagementSystem/src/test/java/com/salarymanagement/in/SalaryManagementSystem/SalaryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salarymanagement.in.SalaryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
